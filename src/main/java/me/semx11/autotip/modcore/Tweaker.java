67309674b91dc6f7ec2987ae0d0c0d39ad191da5
Mitchell Katz <Sk1er@Sk1er.club>
2020-05-04 13:40:52 -0400

    Update build.gradle and modify Tweaker to allow mod to load in production environment

--- src/main/java/me/semx11/autotip/modcore/Tweaker.java
+++ src/main/java/me/semx11/autotip/modcore/Tweaker.java
@@ -1,10 +1,14 @@
 package me.semx11.autotip.modcore;
 
-import me.semx11.autotip.universal.UniversalUtil;
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraftforge.common.ForgeVersion;
+import net.minecraftforge.fml.relauncher.CoreModManager;
 
 import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.CodeSource;
 import java.util.List;
 
 public class Tweaker implements ITweaker {
@@ -22,9 +26,30 @@ public class Tweaker implements ITweaker {
             System.out.println("Autotip not initializing ModCore because AUTOTIP_SKIP_MODCORE is true.");
             return;
         }
-        //Minecraft Version
-        int initialize = ModCoreInstaller.initialize(gameDir, UniversalUtil.getMinecraftVersion().toString()+"_forge");
-        System.out.println("ModCore Init Status From ExampleMod " + initialize);
+        try {
+            String ver = ((String) ForgeVersion.class.getDeclaredField("mcVersion").get(null));
+            //Minecraft Version
+            int initialize = ModCoreInstaller.initialize(gameDir, ver + "_forge");
+            System.out.println("ModCore Init Status From ExampleMod " + initialize);
+
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        CodeSource codeSource = getClass().getProtectionDomain().getCodeSource();
+        if (codeSource != null) {
+            URL location = codeSource.getLocation();
+            try {
+                File file = new File(location.toURI());
+                if (file.isFile()) {
+                    CoreModManager.getIgnoredMods().remove(file.getName());
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("No CodeSource, if this is not a development environment we might run into problems!");
+            System.out.println(getClass().getProtectionDomain());
+        }
     }
 
     @Override
